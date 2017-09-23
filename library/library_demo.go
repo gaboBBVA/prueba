ac59baeeef1f803db280ca346af2c0ca1c58a985
gaboBBVA <gabriel.ramirez.cazarez@bbva.com>
2017-09-23 11:34:37 -0500

    Update library_demo.go

--- library/library_demo.go
+++ library/library_demo.go
@@ -1,6 +1,9 @@
+// Package library
+// Demo de creación de librerías y su forma de compartirlas
 package library
 
-
+// Prueba Libreria
+// Permite demostrar la reutilizacion de libreras compartidas con la comunidad
 func Prueba_libreria (mensaje_entrada string) string {
 	return "Hola " +  mensaje_entrada + " esta libreria esta hecha por Gabriel"
 }
